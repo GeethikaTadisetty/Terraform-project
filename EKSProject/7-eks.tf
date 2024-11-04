f4e5cd824614efb19778bfefe652e6f9ea88f2fd
GeethikaTadisetty <116942901+GeethikaTadisetty@users.noreply.github.com>
2024-11-04 06:30:15 -0800

    Update 7-eks.tf

--- EKSProject/7-eks.tf
+++ EKSProject/7-eks.tf
@@ -1,1 +1,45 @@
+resource "aws_iam_role" "eks"{
+  name = "$[local.env}-${local.eks_name}-eks-cluster"
 
+  assume_role_policy = <<POLICY {
+    "Version" = "2012-10-17",
+    "statement" = [
+  {
+    "Effect" : "Allow",
+    "Action" : "sts:AssumeRole",
+    "Principal" : {
+       "service" : "eks.amazonaws.com
+    }
+  }
+]
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks"{
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.eks.name
+}
+
+resource "aws_eks_cluster" "eks"{
+  name = "${local.env}-${local.eks_name}"
+  role_arn = aws_iam_role_eks.arn
+  version = local.eks_version
+
+  vpc_config {
+      Enable_private_access = false
+      Enable_public_access = true
+
+      subnets = [
+        aws_subnets.private_subnet1.zonel.id,
+        aws_subnets.private_subnet2.zone2.id
+      ]
+   }
+  
+    access_config {
+        authentication_mode = "API"
+        bootstrap_cluster_creator_admin_permissions = true
+    }
+
+depends_on = [aws_iam_role_policy_attachment.eks]
+}
+   
