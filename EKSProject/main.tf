27a02cfd215400f8292797f9c056b03d3ec62553
GeethikaTadisetty <116942901+GeethikaTadisetty@users.noreply.github.com>
2025-02-14 18:38:15 +0530

    Update main.tf

--- EKSProject/main.tf
+++ EKSProject/main.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "ec2_sg" {
 
 # Create an EC2 Instance
 resource "aws_instance" "my_ec2" {
-  #ami                    = "ami-0c55b159cbfafe1f0"  # Ubuntu 22.04 LTS
+  ami                    = "ami-04681163a08179f28"  
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
