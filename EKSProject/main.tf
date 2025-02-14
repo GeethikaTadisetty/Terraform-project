33ace671be0716dba4166933ebc5a3731d2f8187
GeethikaTadisetty <116942901+GeethikaTadisetty@users.noreply.github.com>
2025-02-14 04:00:42 -0800

    Update main.tf

--- EKSProject/main.tf
+++ EKSProject/main.tf
@@ -1,1 +1,45 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+# Create a Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH and HTTP traffic"
+
+  # Allow SSH (Port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP (Port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound tr
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-0c55b159cbfafe1f0"  # Ubuntu 22.04 LTS
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "My-Terraform-EC2"
+  }
+}
