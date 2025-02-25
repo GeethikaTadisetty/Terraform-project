e623370867e01b1e1fbbab23ec118f53443c7e8c
GeethikaTadisetty <116942901+GeethikaTadisetty@users.noreply.github.com>
2025-02-25 09:58:45 +0530

    Update main.tf

--- EKSProject/main.tf
+++ EKSProject/main.tf
@@ -79,83 +79,85 @@ resource "aws_security_group" "ec2_sg" {
  ######## EKS CREATION #########
 
 #creating IAM role
-resource "aws_iam_role" "eks_role" {
-  name = "eks-role"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
       }
-    ]
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-#Binding IAm role to policy
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
 
-resource "aws_iam_role_policy_attachment" "eks_vpc_policy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-}
+#Creating EKS cluster
 
-#creating EKS cluster
-
-resource "aws_eks_cluster" "eks_cluster" {
+resource "aws_eks_cluster" "my_eks_cluster" {
   name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_role.arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.eks_subnet.id]
-    security_group_ids = [aws_security_group.eks_sg.id]
+    subnet_ids = [aws_subnet.mysubnet.id] # Use your public/private subnets
   }
 
-  depends_on = [aws_iam_role_policy_attachment.eks_policy]
+  tags = {
+    Name = "My-EKS-Cluster"
+  }
 }
 
-#creating IAm role and creating nodes
+#Create IAM Role for Worker Nodes
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+##Create EKS Node Group (Worker Nodes)
 
 resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_worker_role.arn
-  subnet_ids      = [aws_subnet.eks_subnet.id]
+  cluster_name  = aws_eks_cluster.my_eks_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn = aws_iam_role.eks_node_role.arn
+
+  subnet_ids = [aws_subnet.mysubnet.id]  # Associate nodes with subnets
+  instance_types = ["t3.medium"]
 
   scaling_config {
     desired_size = 2
-    max_size     = 3
     min_size     = 1
+    max_size     = 3
   }
 
-  instance_types = ["t3.medium"]
+  tags = {
+    Name = "EKS-Worker-Nodes"
+  }
 }
 
-resource "aws_iam_role" "eks_worker_role" {
-  name = "eks-worker-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
 
-resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
-  role       = aws_iam_role.eks_worker_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
 
